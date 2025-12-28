47d8ed3237f738d852c39d07f7af356c25a614f3
Christian Sciberras <christian@yumpu.com>
2025-12-28 23:19:48 +0100

    Add a few interesting platforms

--- phpstan.php
+++ phpstan.php
@@ -3,6 +3,7 @@
 return [
     'parameters' => [
         'parallel' => [
+            'jobSize' => 2,
             'maximumNumberOfProcesses' => max((int)getenv('PROCESSES'), 1),
             'minimumNumberOfJobsPerProcess' => max((int)getenv('PROCESSES'), 1),
         ],
